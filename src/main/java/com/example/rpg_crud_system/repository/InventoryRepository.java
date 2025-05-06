7b6f2f25673384a6b76556f6ec311142f4d8ffb8
merry <rozzimario18@gmail.com>
2025-05-06 11:45:36 +0200

    created repository for inventory

--- src/main/java/com/example/rpg_crud_system/repository/InventoryRepository.java
+++ src/main/java/com/example/rpg_crud_system/repository/InventoryRepository.java
@@ -1,4 +1,10 @@
 package com.example.rpg_crud_system.repository;
 
-public class InventoryRepository {
+import com.example.rpg_crud_system.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
 }
