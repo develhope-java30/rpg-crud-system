2ebd1deb529ff27fe9dfb56cc9e6b8c74ae8c84d
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-05-08 12:12:50 +0200

    add relaxion to CharacterEntity and Inventory

--- src/main/java/com/example/rpg_crud_system/entity/CharacterEntity.java
+++ src/main/java/com/example/rpg_crud_system/entity/CharacterEntity.java
@@ -1,11 +1,9 @@
 package com.example.rpg_crud_system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class CharacterEntity {
