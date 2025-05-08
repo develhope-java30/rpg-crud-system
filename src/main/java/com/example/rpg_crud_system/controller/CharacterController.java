9f781a3c280f02ad4f3cf0f5d70681bfb3cb035b
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-05-08 11:22:09 +0200

    create Character entity repository, service and controller

--- src/main/java/com/example/rpg_crud_system/controller/CharacterController.java
+++ src/main/java/com/example/rpg_crud_system/controller/CharacterController.java
@@ -1,14 +1,42 @@
 package com.example.rpg_crud_system.controller;
 
+import com.example.rpg_crud_system.entity.CharacterEntity;
 import com.example.rpg_crud_system.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/characters")
 public class CharacterController {
 
     @Autowired
-    CharacterService characterService;
+    private CharacterService characterService;
+
+    @PostMapping
+    public CharacterEntity createCharacter(@RequestBody CharacterEntity characterEntity) {
+        return characterService.createCharacter(characterEntity);
+    }
+
+    @GetMapping
+    public List<CharacterEntity> getAll() {
+        return characterService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CharacterEntity> getById(@PathVariable Long id) {
+        return characterService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}/level")
+    public ResponseEntity<CharacterEntity> updateCharacterLevel(@PathVariable Long id, @RequestParam Integer level) {
+        return characterService.updateCharacterLevel(id, level)
+                .map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/example/rpg_crud_system/service/CharacterService.java
+++ src/main/java/com/example/rpg_crud_system/service/CharacterService.java
@@ -1,12 +1,43 @@
 package com.example.rpg_crud_system.service;
 
+import com.example.rpg_crud_system.entity.CharacterEntity;
 import com.example.rpg_crud_system.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CharacterService {
 
     @Autowired
-    CharacterRepository characterRepository;
+    private CharacterRepository characterRepository;
+
+    public CharacterEntity createCharacter(CharacterEntity characterEntity) {
+        return characterRepository.save(characterEntity);
+    }
+
+    public List<CharacterEntity> getAll() {
+        return characterRepository.findAll();
+    }
+
+    public Optional<CharacterEntity> getById(Long id) {
+        return characterRepository.existsById(id) ? characterRepository.findById(id) : Optional.empty();
+    }
+
+    public Optional<CharacterEntity> updateCharacterLevel(Long id, Integer level) {
+        Optional<CharacterEntity> characterEntityOptional = characterRepository.findById(id);
+
+        if (characterEntityOptional.isPresent()) {
+            characterEntityOptional.get().setLevel(level);
+            return Optional.of(characterRepository.save(characterEntityOptional.get()));
+        }
+
+        return Optional.empty();
+    }
+
+    public void deleteCharacter(Long id) {
+        characterRepository.deleteById(id);
+    }
 }
