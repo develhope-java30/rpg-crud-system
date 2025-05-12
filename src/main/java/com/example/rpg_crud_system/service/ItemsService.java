8f5ff3cfd60fd6bbf17abe789b2056aab03ee834
merry <rozzimario18@gmail.com>
2025-05-12 14:28:37 +0200

    added service for items with methods

---
+++ src/main/java/com/example/rpg_crud_system/service/ItemsService.java
@@ -0,0 +1,31 @@
+package com.example.rpg_crud_system.service;
+
+import com.example.rpg_crud_system.entity.ItemsEntity;
+import com.example.rpg_crud_system.repository.ItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemsService {
+    @Autowired
+    private ItemsRepository itemsRepository;
+
+    public List<ItemsEntity> allItems(){
+        return itemsRepository.findAll();
+    }
+
+    public Optional<ItemsEntity> itemFindById(Long id){
+        return itemsRepository.findById(id);
+    }
+
+    public ItemsEntity addItems(ItemsEntity newItem){
+        return itemsRepository.save(newItem);
+    }
+
+    public void deleteItems(Long id){
+        itemsRepository.deleteById(id);
+    }
+}
