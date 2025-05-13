98145ed0e6e9bd61e632606e641fddb1187e05d5
DjGuruge <djguruge@hotmail.it>
2025-05-13 12:30:22 +0200

    addeded crud charStat

--- src/main/java/com/example/rpg_crud_system/controller/CharacterStatsController.java
+++ src/main/java/com/example/rpg_crud_system/controller/CharacterStatsController.java
@@ -1,12 +1,16 @@
 package com.example.rpg_crud_system.controller;
 
+import com.example.rpg_crud_system.entity.CharacterEntity;
 import com.example.rpg_crud_system.entity.CharacterStats;
+import com.example.rpg_crud_system.entity.Inventory;
+import com.example.rpg_crud_system.entity.ItemsEntity;
 import com.example.rpg_crud_system.service.CharacterStatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/CharacterStats")
@@ -22,6 +26,23 @@ public class CharacterStatsController {
     @PostMapping
     public ResponseEntity<CharacterStats> addCharacterStats(@RequestBody CharacterStats newCharacterStats){
         return ResponseEntity.ok(service.addCharacterStats(newCharacterStats));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CharacterStats> getById(@PathVariable Long id) {
+        return service.getById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterStats> updateCharacterStats(@PathVariable Long id, @RequestBody CharacterStats characterStatsToUpdate){
+        Optional<CharacterStats> updatedCharacterStats = service.updateCharacterStats(id, characterStatsToUpdate);
+
+        return updatedCharacterStats
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/example/rpg_crud_system/repository/CharacterStatsRepository.java
+++ src/main/java/com/example/rpg_crud_system/repository/CharacterStatsRepository.java
@@ -2,6 +2,8 @@ package com.example.rpg_crud_system.repository;
 
 import com.example.rpg_crud_system.entity.CharacterStats;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CharacterStatsRepository extends JpaRepository<CharacterStats, Long> {
 }
